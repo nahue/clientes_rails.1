d9e969a001528a41dae5d28b95c39efc62d0ea27
Nahuel Chaves <nahuel.chaves@gmail.com>
2014-08-13 18:13:55 -0300

    mail con mandrill

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,10 +13,18 @@ Rails.application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
-  config.action_mailer.delivery_method = :sendmail
+
   # Don't care if the mailer can't send.
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address   => "smtp.mandrillapp.com",
+    :port      => 25,
+    :user_name => ENV["MANDRILL_USERNAME"],
+    :password  => ENV["MANDRILL_API_KEY"]
+  }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
