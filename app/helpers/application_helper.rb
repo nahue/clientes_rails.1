22500e71014c740c87a5f691d1cacf32da236897
Nahuel Chaves <nahuel.chaves@gmail.com>
2014-08-12 23:56:05 -0300

    estilo

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,9 @@ module ApplicationHelper
     def appname
         "wip"
     end
+
+    def avatar_url(user, size)
+        gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
+        "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{size}"
+    end
 end
