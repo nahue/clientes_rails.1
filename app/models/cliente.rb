b98ca7fcef659ef3d8435b28dde9269388ff5aa2
Nahuel Chaves <nahuel.chaves@gmail.com>
2014-08-14 23:38:11 -0300

    font awesome, export to csv y xls

--- app/models/cliente.rb
+++ app/models/cliente.rb
@@ -2,4 +2,14 @@ class Cliente < ActiveRecord::Base
   belongs_to :user
   has_many :trabajos
   validates_formatting_of :email, using: :email
+
+  def self.to_csv
+    CSV.generate do |csv|
+        csv << column_names
+        all.each do |cliente|
+            csv << cliente.attributes.values_at(*column_names)
+        end
+    end
+  end
 end
+
