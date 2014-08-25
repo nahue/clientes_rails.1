75b5cd5b52f6f8191355144be24435830a76600c
Nahuel Chaves <nahuel.chaves@gmail.com>
2014-08-25 15:44:22 -0300

    cambios en mailer para que aparezca el mail del usuario que lo envío, y la respuesta sea enviada hacia el

--- app/mailers/trabajo_mailer.rb
+++ app/mailers/trabajo_mailer.rb
@@ -2,10 +2,11 @@ class TrabajoMailer < ActionMailer::Base
   default from: "info@nahuel-chaves.com.ar"
 
   def detalle_trabajo(usuario, cliente, trabajo)
+    @usuario = usuario
     @cliente = cliente
     @trabajo = trabajo
 
-    mail(to: [@cliente.email, usuario.email], subject: "Nuevo trabajo asignado")
+    mail(to: [@cliente.email, usuario.email], reply_to: usuario.email, subject: "Nuevo trabajo asignado")
   end
 
 end
