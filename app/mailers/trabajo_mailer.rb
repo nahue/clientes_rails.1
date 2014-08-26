9a410d1da913f8d533666a903a673e081d51a725
Nahuel Chaves <nahuel.chaves@gmail.com>
2014-08-26 14:18:43 -0300

    mejoras en el mailer

--- app/mailers/trabajo_mailer.rb
+++ app/mailers/trabajo_mailer.rb
@@ -1,12 +1,20 @@
 class TrabajoMailer < ActionMailer::Base
-  default from: "info@nahuel-chaves.com.ar"
 
-  def detalle_trabajo(usuario, cliente, trabajo)
-    @usuario = usuario
-    @cliente = cliente
-    @trabajo = trabajo
 
-    mail(to: [@cliente.email, usuario.email], reply_to: usuario.email, subject: "Nuevo trabajo asignado")
-  end
+    #default from: "info@nahuel-chaves.com.ar"
+
+    def detalle_trabajo(usuario, cliente, trabajo)
+        @usuario = usuario
+        @cliente = cliente
+        @trabajo = trabajo
+        dominio = usuario.email.split("@")[1]
+        mail(
+             "to" => [@cliente.email, usuario.email],
+             "from" => usuario.email,
+             "reply_to" => usuario.email,
+             "subject" => "Nuevo trabajo asignado",
+             "Message-ID" => "#{Digest::SHA2.hexdigest(Time.now.to_i.to_s)}@{dominio}"
+        )
+    end
 
 end
