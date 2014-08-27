947c70be990789d3f3d58dc8a508dfdd92701db3
Nahuel Chaves <nahuel.chaves@gmail.com>
2014-08-27 19:27:02 -0300

    smtp settings to .env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,11 +67,11 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :authentication => :plain,
-    :address => "***REMOVED***",
+    :address => ENV["SMTP_HOST"],
     :port => 587,
-    :domain => "***REMOVED***",
-    :user_name => "***REMOVED***",
-    :password => "***REMOVED***"
+    :domain => ENV["DOMAIN"],
+    :user_name => ENV["SMTP_USERNAME"],
+    :password => ENV["SMTP_PASSWORD"]
   }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
