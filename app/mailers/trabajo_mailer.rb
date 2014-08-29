52ceb45afb2502828daf1fa0fd853db625e856ba
Nahuel Chaves <nahuel.chaves@gmail.com>
2014-08-29 12:42:03 -0300

    fix en mailer, muestra imagen en form

--- app/mailers/trabajo_mailer.rb
+++ app/mailers/trabajo_mailer.rb
@@ -5,19 +5,20 @@ class TrabajoMailer < ActionMailer::Base
     #default from: "info@nahuel-chaves.com.ar"
 
     def detalle_trabajo(usuario, cliente, trabajo)
-        if usuario.get_preferencia(:notificar_cliente)
+        to = usuario.get_preferencia(:notificar_cliente) ? [@cliente.email, usuario.email] : usuario.email
+        #if usuario.get_preferencia(:notificar_cliente)
             @usuario = usuario
             @cliente = cliente
             @trabajo = trabajo
             dominio = usuario.email.split("@")[1]
             mail(
-                 "to" => [@cliente.email, usuario.email],
+                 "to" => to,
                  "from" => usuario.email,
                  "reply_to" => usuario.email,
                  "subject" => "Nuevo trabajo asignado",
                  "Message-ID" => "#{Digest::SHA2.hexdigest(Time.now.to_i.to_s)}@#{dominio}"
             )
-        end
+        #end
     end
 
 end
